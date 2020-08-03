5ca3c0c3141197c36f43e6b6e31883b2c9559eb0
Adam McKnight <adammcknight@me.com>
2020-08-03 16:17:46 -0400

    Added base components to the project.

--- app/src/pages/Tab1.tsx
+++ app/src/pages/Tab1.tsx
@@ -1,23 +1,60 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonCard, 
+  IonCardHeader, 
+  IonCardSubtitle, 
+  IonCardTitle, 
+  IonCardContent 
+} from '@ionic/react';
 import './Tab1.css';
+import Greeting from '../components/Greeting';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Hello</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">
+              <Greeting/>
+            </IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Featured Prompts</IonCardSubtitle>
+            <IonCardTitle>Prompt One</IonCardTitle>
+            <IonCardContent>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quibusdam!</IonCardContent>
+          </IonCardHeader>
+        </IonCard>
+  
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Featured Prompts</IonCardSubtitle>
+            <IonCardTitle>Prompt Two</IonCardTitle>
+            <IonCardContent>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quibusdam!</IonCardContent>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Featured Prompts</IonCardSubtitle>
+            <IonCardTitle>Prompt Three</IonCardTitle>
+            <IonCardContent>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quibusdam!</IonCardContent>
+          </IonCardHeader>
+        </IonCard>
+
       </IonContent>
     </IonPage>
   );
