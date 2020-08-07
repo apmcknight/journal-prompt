520a5e83328163393c3f5e2954b42570e8287ee0
Adam McKnight <adammcknight@me.com>
2020-08-07 01:11:10 -0400

    Added local json data to the app.

--- app/src/pages/Tab1.tsx
+++ app/src/pages/Tab1.tsx
@@ -1,7 +1,4 @@
-import React, {
-  useState,
-  useEffect
-} from 'react';
+import React from 'react';
 import {
   IonContent, 
   IonHeader, 
@@ -16,60 +13,57 @@ import {
 import './Tab1.css';
 import Greeting from '../components/Greeting';
 import TutorialHero from '../assets/tutorial-image.jpg';
+import PromptData from '../assets/db.json';
 
+ 
 const Tab1: React.FC = () => {
-  const [promptId, setPromptId] = useState('');
-  const [promptTitle, setPromptTitle] = useState('');
-  const [promptCatagory, setPromptCatagory] = useState('');
-  const [prompContent, setPromptContent] = useState('');
+    return (
   
-  useEffect(() => {
-    fetch("http://192.168.1.2:8080/api")
-      .then (res => res.json())
-      .then(data => {
-        console.log(data);
-      })
-  }, [])
-
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>j-prompt.</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">
-              <Greeting/>
-            </IonTitle>
+            <IonTitle>j-prompt.</IonTitle>
           </IonToolbar>
         </IonHeader>
-          
-        <IonCard>
+        <IonContent>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">
+                <Greeting/>
+              </IonTitle>
+            </IonToolbar>
+          </IonHeader>
+            
+          <IonCard>
           <IonCardHeader>
-            <IonCardTitle>Randomize</IonCardTitle>
-          </IonCardHeader>
-        </IonCard>
+              <IonCardSubtitle>FEATURED PROMPT</IonCardSubtitle>
+    <IonCardTitle>{PromptData.prompts.sports[0].title}</IonCardTitle>
+            </IonCardHeader>
+          </IonCard>
 
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Prompt Catagories</IonCardTitle>
-          </IonCardHeader>
-        </IonCard>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardTitle>Randomize</IonCardTitle>
+            </IonCardHeader>
+          </IonCard>
 
-        <IonCard>
-          <img src={TutorialHero} alt=""/>
-          <IonCardHeader>
-            <IonCardSubtitle>Tutorial</IonCardSubtitle>
-            <IonCardTitle>Learn Journal Prompt</IonCardTitle>
-          </IonCardHeader>
-        </IonCard>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardTitle>Prompt Catagories</IonCardTitle>
+            </IonCardHeader>
+          </IonCard>
 
-      </IonContent>
-    </IonPage>
-  );
+          <IonCard>
+            <img src={TutorialHero} alt=""/>
+            <IonCardHeader>
+              <IonCardSubtitle>Tutorial</IonCardSubtitle>
+              <IonCardTitle>Learn Journal Prompt</IonCardTitle>
+            </IonCardHeader>
+          </IonCard>
+
+        </IonContent>
+      </IonPage>
+    );
 };
 
 export default Tab1;
